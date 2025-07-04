104f0631cc86526d912a2b20d0ae418b3fd47bb9
bringonweb <adityagupta@bringon.info>
2025-07-04 20:09:56 +0530

    added versioning to index js in index.php

--- index.php
+++ index.php
@@ -584,7 +584,7 @@
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
-    <script src="js/index.js"></script>
+    <script src="js/index.js?v=1.0.1"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             const header = document.getElementById('header');
