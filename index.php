99acdef5544748876b65cf66dee3d27e08be8d5c
bringonweb <adityagupta@bringon.info>
2025-07-04 20:42:07 +0530

    added versioning to css as well in index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" />
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index.css?v=1.0.1">
     <link rel="stylesheet" href="css/footer.css">
 
 </head>
